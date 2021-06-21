1416d21dcc948eba23d665e0bc40c337f286fba8
Peter <52173034+Ainzll@users.noreply.github.com>
2021-06-21 14:34:36 -0700

    Add files via upload

---
+++ WebInitials.py
@@ -0,0 +1,20 @@
+#Initials
+print("PPPPP   V             V")
+print("P    P   V           V")
+print("P    P    V         V")
+print("P    P     V       V")
+print("PPPPP       V     V")
+print("P            V   V")
+print("P             V V")
+print("P              V")
+
+#advanced
+line1 = "\t PPPPP   V             V \n"
+line2 = "\t P    P   V           V \n"
+line3 = "\t P    P    V         V \n"
+line4 = "\t P    P     V       V \n"
+line5 = "\t PPPPP       V     V \n"
+line6 = "\t P            V   V \n"
+line7 = "\t P             V V \n"
+line8 = "\t P              V \n"
+print(line1, line2, line3, line4, line5, line6, line7, line8)
